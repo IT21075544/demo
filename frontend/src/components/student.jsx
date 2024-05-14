f55478f2717d545aec5289311a95a79342464ccf
Heshan Jayakodi <99264803+IT21075544@users.noreply.github.com>
2024-05-14 07:45:05 +0530

    impt

--- frontend/src/components/student.jsx
+++ frontend/src/components/student.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Chart, { Colors } from "chart.js/auto";
+//import Chart, { Colors } from "chart.js/auto";
 
 // Import your background image
 import backgroundImage from "./background.jpg";
